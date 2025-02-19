8e6224b9cd78f09f33e4742f74986319b289ab78
opoulin70 <opoulin70@gmail.com>
2025-02-18 19:38:46 -0500

    Added forgotten root CMakeLists.txt

--- src/EventMonitor/test/Device.test.cpp
+++ src/EventMonitor/test/Device.test.cpp
@@ -1,5 +1,5 @@
 #include "gtest/gtest.h"
-#include <EventMonitor/Device.h>
+#include "../Device.h"
 
 TEST(DeviceTests, CreateDevice) {
     Device dev = Device::CreateFromSyspath("/sys/devices/pci0000:00/0000:00:0c.0/usb1/1-1");
